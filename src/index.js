614eef9ab7008531c09c1b8ca4f776d8412f6c7e
ichigrinova <ichigrinova@calltouch.net>
2022-08-20 09:44:44 +0300

    second step added

--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@ export default () => {
         .name('gendiff')
         .description('Compares two configuration files and shows a difference.')
         .version('0.0.1')
-        .parse();
+        .arguments('<filepath1> <filepath2>')
+        .option('-f, --format <type>', 'output format');
+
+    if(process.argv.includes('--help') || process.argv.includes('-h')) {
+        console.log(program.help());
+    } else if (process.argv.includes('--version') || process.argv.includes('-V')) {
+        console.log(program.version());
+    } else {
+        program.parse();
+    }
 }
