e0f93e5da18ab8f0bebe99db04532da57af08060
ichigrinova <ichigrinova@calltouch.net>
2022-08-23 16:45:16 +0300

    typo fixed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2, program.opts().format || 'stylish');
+    const diff = genDiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
   });
 
